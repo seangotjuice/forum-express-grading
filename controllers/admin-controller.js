58b021b5f8a50aae819d0d676e4f87c6e03c7ab9
Sean Chen <seanchen112@gmail.com>
2023-06-21 23:53:39 +0800

    feat: show category on admin restaurant pages

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,29 +1,29 @@
-const { Restaurant } = require('../models') // 新增這裡
-const { localFileHandler } = require('../helpers/file-helpers') // 將 file-helper 載進來
+const { Restaurant, User, Category } = require("../models"); // 新增這裡
+const { localFileHandler } = require("../helpers/file-helpers"); // 將 file-helper 載進來
 
 const adminController = {
-  // 修改這裡
-
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category],
     })
 
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .then((restaurants) => res.render("admin/restaurants", { restaurants }))
 
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   createRestaurant: (req, res) => {
-    return res.render('admin/create-restaurant')
+    return res.render("admin/create-restaurant");
   },
 
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
-    if (!name) throw new Error('Restaurant name is required!')
+    const { name, tel, address, openingHours, description } = req.body;
+    if (!name) throw new Error("Restaurant name is required!");
     // 修改以下
-    const { file } = req // 把檔案取出來，也可以寫成 const file = req.file
+    const { file } = req; // 把檔案取出來，也可以寫成 const file = req.file
     localFileHandler(file) // 把取出的檔案傳給 file-helper 處理後
-      .then(filePath =>
+      .then((filePath) =>
         Restaurant.create({
           // 再 create 這筆餐廳資料
           name,
@@ -31,49 +31,51 @@ const adminController = {
           address,
           openingHours,
           description,
-          image: filePath || null
+          image: filePath || null,
         })
       )
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully created')
-        res.redirect('/admin/restaurants')
+        req.flash("success_messages", "restaurant was successfully created");
+        res.redirect("/admin/restaurants");
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
       // 去資料庫用 id 找一筆資料
-      raw: true // 找到以後整理格式再回傳
+      raw: true, // 找到以後整理格式再回傳
+      nest: true,
+      include: [Category],
     })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
-        res.render('admin/restaurant', { restaurant })
+      .then((restaurant) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!"); //  如果找不到，回傳錯誤訊息，後面不執行
+        res.render("admin/restaurant", { restaurant });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   editRestaurant: (req, res, next) => {
     // 新增這段
     Restaurant.findByPk(req.params.id, {
-      raw: true
+      raw: true,
     })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('admin/edit-restaurant', { restaurant })
+      .then((restaurant) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!");
+        res.render("admin/edit-restaurant", { restaurant });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
-    if (!name) throw new Error('Restaurant name is required!')
-    const { file } = req // 把檔案取出來
+    const { name, tel, address, openingHours, description } = req.body;
+    if (!name) throw new Error("Restaurant name is required!");
+    const { file } = req; // 把檔案取出來
     Promise.all([
       // 非同步處理
       Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
-      localFileHandler(file) // 把檔案傳到 file-helper 處理
+      localFileHandler(file), // 把檔案傳到 file-helper 處理
     ])
       .then(([restaurant, filePath]) => {
         // 以上兩樣事都做完以後
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        if (!restaurant) throw new Error("Restaurant didn't exist!");
         return restaurant.update({
           // 修改這筆資料
           name,
@@ -81,25 +83,25 @@ const adminController = {
           address,
           openingHours,
           description,
-          image: filePath || restaurant.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
-        })
+          image: filePath || restaurant.image, // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
+        });
       })
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully to update')
-        res.redirect('/admin/restaurants')
+        req.flash("success_messages", "restaurant was successfully to update");
+        res.redirect("/admin/restaurants");
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   deleteRestaurant: (req, res, next) => {
     // 新增以下
     return Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return restaurant.destroy()
+      .then((restaurant) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!");
+        return restaurant.destroy();
       })
-      .then(() => res.redirect('/admin/restaurants'))
-      .catch(err => next(err))
-  }
-}
+      .then(() => res.redirect("/admin/restaurants"))
+      .catch((err) => next(err));
+  },
+};
 
-module.exports = adminController
+module.exports = adminController;
