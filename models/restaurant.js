3c5ec558f45f6ac014a44da8fb509ea022a9e552
Sean Chen <seanchen112@gmail.com>
2023-06-21 21:05:17 +0800

    feat: add category model

--- models/restaurant.js
+++ models/restaurant.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      // define association here
+      Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' }) // 增加這行
     }
   }
   Restaurant.init(
@@ -18,13 +18,11 @@ module.exports = (sequelize, DataTypes) => {
       address: DataTypes.STRING,
       openingHours: DataTypes.STRING,
       description: DataTypes.TEXT,
-      image: DataTypes.STRING // 新增這一行
+      image: DataTypes.STRING
     },
     {
       sequelize,
       modelName: 'Restaurant',
-      tableName: 'Restaurants', // 新增這裡
-
       underscored: true
     }
   )
