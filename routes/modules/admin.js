b1912dfa539704aec7a9fe814519e11f5717cb6a
Sean Chen <seanchen112@gmail.com>
2023-06-24 15:50:33 +0800

    feat: category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -26,6 +26,7 @@ router.post(
 )
 router.get('/categories/:id', categoryController.getCategories) // 新增這行
 router.put('/categories/:id', categoryController.putCategory) // 新增這行
+router.delete('/categories/:id', categoryController.deleteCategory) // 新增這行
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
