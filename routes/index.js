bb31a5782803c9f890a9cde8db4466aeaf0aa90d
Sean Chen <seanchen112@gmail.com>
2023-06-24 17:25:26 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const router = express.Router()
-const admin = require('./modules/admin')
-const restController = require('../controllers/restaurant-controller')
-const userController = require('../controllers/user-controller') // 新增這行
-const { authenticated } = require('../middleware/auth') // 引入 auth.js
+const express = require("express");
+const router = express.Router();
+const admin = require("./modules/admin");
+const restController = require("../controllers/restaurant-controller");
+const userController = require("../controllers/user-controller"); // 新增這行
+const { authenticated } = require("../middleware/auth"); // 引入 auth.js
 
-const { generalErrorHandler } = require('../middleware/error-handler') // 加入這行
-const passport = require('../config/passport')
+const { generalErrorHandler } = require("../middleware/error-handler"); // 加入這行
+const passport = require("../config/passport");
+const restaurantController = require("../controllers/restaurant-controller");
 
-router.use('/admin', admin)
-router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp) // 注意用 post
-router.get('/signin', userController.signInPage)
+router.use("/admin", admin);
+router.get("/signup", userController.signUpPage);
+router.post("/signup", userController.signUp); // 注意用 post
+router.get("/signin", userController.signInPage);
 router.post(
-  '/signin',
-  passport.authenticate('local', {
-    failureRedirect: '/signin',
-    failureFlash: true
+  "/signin",
+  passport.authenticate("local", {
+    failureRedirect: "/signin",
+    failureFlash: true,
   }),
   userController.signIn
-) // 注意是 post
-router.get('/logout', userController.logout)
-router.get('/restaurants', authenticated, restController.getRestaurants)
-router.get('/', (req, res) => res.redirect('/restaurants'))
-router.use('/', generalErrorHandler) // 加入這行
+); // 注意是 post
+router.get("/logout", userController.logout);
+router.get("/restaurants", authenticated, restController.getRestaurants);
+router.get("/restaurants/:id", authenticated, restController.getRestaurant); // 新增這行
 
-module.exports = router
+router.get("/", (req, res) => res.redirect("/restaurants"));
+router.use("/", generalErrorHandler); // 加入這行
+
+module.exports = router;
