b4f901c7cbd378a3901979ba5629b6a5f653980a
Sean Chen <seanchen112@gmail.com>
2023-06-26 16:45:36 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,51 +1,61 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category } = require("../models");
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
+    const categoryId = Number(req.query.categoryId) || ""; // 新增這裡，從網址上拿下來的參數是字串，先轉成 Number 再操作
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          // 新增查詢條件
+          ...(categoryId ? { categoryId } : {}), // 檢查 categoryId 是否為空值
+        },
+        nest: true,
+        raw: true,
+      }),
+      Category.findAll({ raw: true }),
+    ]).then(([restaurants, categories]) => {
+      const data = restaurants.map((r) => ({
         ...r,
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', {
-        restaurants: data
-      })
-    })
+        description: r.description.substring(0, 50),
+      }));
+      return res.render("restaurants", {
+        restaurants: data,
+        categories,
+        categoryId, // 新增這裡
+      });
+    });
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
-      nest: true
+      nest: true,
     })
-      .then(restaurant => {
+      .then((restaurant) => {
         if (!restaurant) {
-          throw new Error("Restaurant didn't exist in detail!")
+          throw new Error("Restaurant didn't exist in detail!");
         }
-        return restaurant.increment('viewCounts', { by: 1 })
+        return restaurant.increment("viewCounts", { by: 1 });
       })
-      .then(restaurant => {
-        return res.render('restaurant', { restaurant: restaurant.toJSON() })
+      .then((restaurant) => {
+        return res.render("restaurant", { restaurant: restaurant.toJSON() });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
       nest: true,
-      raw: true
+      raw: true,
     })
-      .then(restaurant => {
+      .then((restaurant) => {
         if (!restaurant) {
-          throw new Error("Restaurant didn't exist in dashboard!")
+          throw new Error("Restaurant didn't exist in dashboard!");
         }
-        return res.render('dashboard', {
-          restaurant
-        })
+        return res.render("dashboard", {
+          restaurant,
+        });
       })
-      .catch(err => next(err))
-  }
-}
-module.exports = restaurantController
+      .catch((err) => next(err));
+  },
+};
+module.exports = restaurantController;
