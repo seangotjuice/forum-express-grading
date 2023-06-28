0050522f6baa4e62ff823aa11ae27a3ea12efd07
Sean Chen <seanchen112@gmail.com>
2023-06-28 12:22:25 +0800

    feat: add favorite/unfavorite button at index page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,48 +1,88 @@
-const bcrypt = require('bcryptjs') // 載入 bcrypt
-const db = require('../models')
-const { User } = db
+const bcrypt = require("bcryptjs"); // 載入 bcrypt
+const db = require("../models");
+const { User, Restaurant, Comment, Favorite } = db;
+// const { User, Restaurant, Comment, Favorite } = require("../models");
+
 const userController = {
   signUpPage: (req, res) => {
-    res.render('signup')
+    res.render("signup");
   },
   signUp: (req, res, next) => {
     // 修改這裡
     // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
     if (req.body.password !== req.body.passwordCheck) {
-      throw new Error('Passwords do not match!')
+      throw new Error("Passwords do not match!");
     }
 
     // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
     User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10) // 前面加 return
+      .then((user) => {
+        if (user) throw new Error("Email already exists!");
+        return bcrypt.hash(req.body.password, 10); // 前面加 return
       })
-      .then(hash =>
+      .then((hash) =>
         User.create({
           // 上面錯誤狀況都沒發生，就把使用者的資料寫入資料庫
           name: req.body.name,
           email: req.body.email,
-          password: hash
+          password: hash,
         })
       )
       .then(() => {
-        req.flash('success_messages', '成功註冊帳號！') // 並顯示成功訊息
-        res.redirect('/signin')
+        req.flash("success_messages", "成功註冊帳號！"); // 並顯示成功訊息
+        res.redirect("/signin");
       })
-      .catch(err => next(err)) // 接住前面拋出的錯誤，呼叫專門做錯誤處理的 middleware
+      .catch((err) => next(err)); // 接住前面拋出的錯誤，呼叫專門做錯誤處理的 middleware
   },
   signInPage: (req, res) => {
-    res.render('signin')
+    res.render("signin");
   },
   signIn: (req, res) => {
-    req.flash('success_messages', '成功登入！')
-    res.redirect('/restaurants')
+    req.flash("success_messages", "成功登入！");
+    res.redirect("/restaurants");
   },
   logout: (req, res) => {
-    req.flash('success_messages', '登出成功！')
-    req.logout()
-    res.redirect('/signin')
-  }
-}
-module.exports = userController
+    req.flash("success_messages", "登出成功！");
+    req.logout();
+    res.redirect("/signin");
+  },
+  addFavorite: (req, res, next) => {
+    const { restaurantId } = req.params;
+    return Promise.all([
+      Restaurant.findByPk(restaurantId),
+      Favorite.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId,
+        },
+      }),
+    ])
+      .then(([restaurant, favorite]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!");
+        if (favorite) throw new Error("You have favorited this restaurant!");
+
+        return Favorite.create({
+          userId: req.user.id,
+          restaurantId,
+        });
+      })
+      .then(() => res.redirect("back"))
+      .catch((err) => next(err));
+  },
+  removeFavorite: (req, res, next) => {
+    return Favorite.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId: req.params.restaurantId,
+      },
+    })
+      .then((favorite) => {
+        if (!favorite) throw new Error("You haven't favorited this restaurant");
+
+        return favorite.destroy();
+      })
+      .then(() => res.redirect("back"))
+      .catch((err) => next(err));
+  },
+};
+module.exports = userController;
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,53 @@
-const express = require('express')
-const router = express.Router()
-const admin = require('./modules/admin')
-const restController = require('../controllers/restaurant-controller')
-const userController = require('../controllers/user-controller') // 新增這行
-const { authenticated, authenticatedAdmin } = require('../middleware/auth') // 引入 auth.js
-const commentController = require('../controllers/comment-controller') // 引入 controller
+const express = require("express");
+const router = express.Router();
+const admin = require("./modules/admin");
+const restController = require("../controllers/restaurant-controller");
+const userController = require("../controllers/user-controller");
+const { authenticated, authenticatedAdmin } = require("../middleware/auth");
+const commentController = require("../controllers/comment-controller");
+const { generalErrorHandler } = require("../middleware/error-handler");
+const passport = require("../config/passport");
 
-const { generalErrorHandler } = require('../middleware/error-handler') // 加入這行
-const passport = require('../config/passport')
-
-router.use('/admin', admin)
-router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp) // 注意用 post
-router.get('/signin', userController.signInPage)
+router.use("/admin", admin);
+router.get("/signup", userController.signUpPage);
+router.post("/signup", userController.signUp);
+router.get("/signin", userController.signInPage);
 router.post(
-  '/signin',
-  passport.authenticate('local', {
-    failureRedirect: '/signin',
-    failureFlash: true
+  "/signin",
+  passport.authenticate("local", {
+    failureRedirect: "/signin",
+    failureFlash: true,
   }),
   userController.signIn
-) // 注意是 post
-router.get('/logout', userController.logout)
+);
+router.get("/logout", userController.logout);
 
-router.get('/restaurants/feeds', authenticated, restController.getFeeds) // 新增這一行
-
+router.get("/restaurants/feeds", authenticated, restController.getFeeds);
 router.get(
-  '/restaurants/:id/dashboard',
+  "/restaurants/:id/dashboard",
   authenticated,
   restController.getDashboard
-)
-router.get('/restaurants/:id', authenticated, restController.getRestaurant) // 新增這行
-router.get('/restaurants', authenticated, restController.getRestaurants)
+);
+router.get("/restaurants/:id", authenticated, restController.getRestaurant);
+router.get("/restaurants", authenticated, restController.getRestaurants);
 
-router.get('/', (req, res) => res.redirect('/restaurants'))
-router.use('/', generalErrorHandler) // 加入這行
+router.get("/", (req, res) => res.redirect("/restaurants"));
+router.use("/", generalErrorHandler);
 router.delete(
-  '/comments/:id',
+  "/comments/:id",
   authenticatedAdmin,
   commentController.deleteComment
-)
-router.post('/comments', authenticated, commentController.postComment) // 加入路由設定
+);
+router.post("/comments", authenticated, commentController.postComment);
+router.post(
+  "/favorite/:restaurantId",
+  authenticated,
+  userController.addFavorite
+);
+router.delete(
+  "/favorite/:restaurantId",
+  authenticated,
+  userController.removeFavorite
+);
 
-module.exports = router
+module.exports = router;
