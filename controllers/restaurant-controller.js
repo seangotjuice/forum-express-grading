8fe1ecceb6e790d6995ab2b5d67127988efb9f33
Sean Chen <seanchen112@gmail.com>
2023-06-28 10:47:40 +0800

    feat: create favorite model

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,98 +1,98 @@
-const { Restaurant, Category, Comment, User } = require("../models"); // 修改這裡
+const { Restaurant, Category, Comment, User } = require('../models') // 修改這裡
 
-const { getOffset, getPagination } = require("../helpers/pagination-helper");
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: (req, res) => {
-    const DEFAULT_LIMIT = 9;
-    const categoryId = Number(req.query.categoryId) || "";
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT;
-    const offset = getOffset(limit, page);
+    const DEFAULT_LIMIT = 9
+    const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
 
     return Promise.all([
       Restaurant.findAndCountAll({
         // 修改這裡
         include: Category,
         where: {
-          ...(categoryId ? { categoryId } : {}),
+          ...(categoryId ? { categoryId } : {})
         },
         limit,
         offset,
         nest: true,
-        raw: true,
+        raw: true
       }),
-      Category.findAll({ raw: true }),
+      Category.findAll({ raw: true })
     ]).then(([restaurants, categories]) => {
-      const data = restaurants.rows.map((r) => ({
+      const data = restaurants.rows.map(r => ({
         // 修改這裡，加上 .rows
         ...r,
-        description: r.description.substring(0, 50),
-      }));
-      return res.render("restaurants", {
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('restaurants', {
         restaurants: data,
         categories,
         categoryId,
-        pagination: getPagination(limit, page, restaurants.count), // 修改這裡，把 pagination 資料傳回樣板
-      });
-    });
+        pagination: getPagination(limit, page, restaurants.count) // 修改這裡，把 pagination 資料傳回樣板
+      })
+    })
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: [Category, { model: Comment, include: User }],
+      include: [Category, { model: Comment, include: User }]
     })
-      .then((restaurant) => {
-        console.log(restaurant.Comments[0].dataValues);
+      .then(restaurant => {
+        console.log(restaurant.Comments[0].dataValues)
         if (!restaurant) {
-          throw new Error("Restaurant didn't exist in detail!");
+          throw new Error("Restaurant didn't exist in detail!")
         }
-        return restaurant.increment("viewCounts", { by: 1 });
+        return restaurant.increment('viewCounts', { by: 1 })
       })
-      .then((restaurant) => {
-        return res.render("restaurant", { restaurant: restaurant.toJSON() });
+      .then(restaurant => {
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
-      .catch((err) => next(err));
+      .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
       nest: true,
-      raw: true,
+      raw: true
     })
-      .then((restaurant) => {
+      .then(restaurant => {
         if (!restaurant) {
-          throw new Error("Restaurant didn't exist in dashboard!");
+          throw new Error("Restaurant didn't exist in dashboard!")
         }
-        return res.render("dashboard", {
-          restaurant,
-        });
+        return res.render('dashboard', {
+          restaurant
+        })
       })
-      .catch((err) => next(err));
+      .catch(err => next(err))
   },
   getFeeds: (req, res, next) => {
     return Promise.all([
       Restaurant.findAll({
         limit: 10,
-        order: [["createdAt", "DESC"]],
+        order: [['createdAt', 'DESC']],
         include: [Category],
         raw: true,
-        nest: true,
+        nest: true
       }),
       Comment.findAll({
         limit: 10,
-        order: [["createdAt", "DESC"]],
+        order: [['createdAt', 'DESC']],
         include: [User, Restaurant],
         raw: true,
-        nest: true,
-      }),
+        nest: true
+      })
     ])
       .then(([restaurants, comments]) => {
-        res.render("feeds", {
+        res.render('feeds', {
           restaurants,
-          comments,
-        });
+          comments
+        })
       })
-      .catch((err) => next(err));
-  },
-};
-module.exports = restaurantController;
+      .catch(err => next(err))
+  }
+}
+module.exports = restaurantController
