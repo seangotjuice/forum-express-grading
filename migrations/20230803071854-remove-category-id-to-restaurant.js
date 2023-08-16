715019661ac2d48b8de1b775ed4314d9ff4f02f3
Sean Chen <seanchen112@gmail.com>
2023-08-16 10:39:39 +0800

    fix: db recreate

--- migrations/20230803071854-remove-category-id-to-restaurant.js
+++ migrations/20230803071854-remove-category-id-to-restaurant.js
@@ -1,18 +1,12 @@
 "use strict";
-
+// 為了補齊R01新出現migration
+// 不然會出現: ERROR: Unable to find migration: 20171125081136-xxxxxx.js
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("Restaurants", "category_id", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: {
-        model: "Categories",
-        key: "id",
-      },
-    });
+  up: function (queryInterface, Sequelize) {
+    return Promise.resolve();
   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("Restaurants", "category_id");
+  down: function (queryInterface) {
+    return Promise.resolve();
   },
 };
